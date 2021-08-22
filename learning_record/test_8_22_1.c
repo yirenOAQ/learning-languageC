327e1f8da12e2047b2c72b28270c8fda6cd00fcf
yirenOAQ <1418466069@qq.com>
2021-08-22 17:58:55 +0800

    2021_8_22学习记录

---
+++ learning_record/test_8_22_1.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+
+//字符串结尾默认自带\0
+
+int main()
+{
+	char arr[] = "hello world";
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c' };
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+	printf("%d\n", strlen(arr1));
+	printf("%d\n", strlen(arr2));
+	return 0;
+}
