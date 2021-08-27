f40d80f666ee5a8fcbcd7f33028fa56e5bfcb9c5
yiren <89143389+yirenOAQ@users.noreply.github.com>
2021-08-27 10:51:04 +0800

    Add files via upload

---
+++ learning_record/2021_8_27/test_8_27_4.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int n = 1;
+	int m = 2;
+	switch (n)
+	{
+	case 1:
+		m++;
+	case 2:
+		n++;
+	case 3:
+		switch (n)
+		{             // switch允许嵌套使用
+		case 1:
+			n++;
+		case 2:
+			m++; n++;
+			break;
+		}
+	case 4:
+		m++;
+		break;
+	default:
+		break;
+	}
+	printf("m = %d, n = %d\n", m, n);   //结果为5和3
+	return 0;
+}
