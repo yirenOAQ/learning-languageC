24a88cc685b3462011808dc776886a9f04ba30d3
yiren <89143389+yirenOAQ@users.noreply.github.com>
2021-08-29 10:27:13 +0800

    Add files via upload

---
+++ learning_record/2021_8_29/test_8_29_03.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+//ÕËºÅµÇÂ¼³ÌĞòÄ£Äâ
+
+int main()
+{
+	char password[20] = { 0 };
+	printf("ÇëÊäÈëÃÜÂë£º\n");
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		scanf("%s", password);
+		if (strcmp(password, "qwerty123456") == 0)
+		{
+			printf("µÇÂ¼³É¹¦\n");
+			break;
+		}
+		else
+		{
+			printf("ÃÜÂë´íÎó£¬ÇëÖØÊÔ\n");
+		}
+	}
+	if (i == 3)
+	{
+		printf("µÇÂ¼Ê§°Ü");
+	}
+	return 0;
+}
