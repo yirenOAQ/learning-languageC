8362473ee342c6a75d83c1276e362e380cea3dd4
yiren <89143389+yirenOAQ@users.noreply.github.com>
2021-11-23 22:45:00 +0800

    Add files via upload

---
+++ 2021/2021_11/2021_11_24/2021_11_24_01.c
@@ -0,0 +1,12 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#define DAYS_FEB(year) ((((year % 4 == 0) && (year % 100 != 0)) || year % 400 == 0) ? 29 : 28)
+
+int main()
+{
+	int year = 0;
+	scanf("%d", &year);
+	year = DAYS_FEB(year);
+	printf("days of the FEB.: %d\n", year);
+	return 0;
+}
